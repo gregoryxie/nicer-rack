d95acb1d23918fb745729865cb1fbab718f02fd4
Tristan Culp <tculp@mit.edu>
2022-04-13 18:33:18 -0400

    Implement mp3 to wav bytestream functionality

--- link_handler.py
+++ link_handler.py
@@ -4,6 +4,9 @@ store information in the database.
 """
 import os
 import pytube
+import wave
+import struct
+from pydub import AudioSegment
 
 MAX_LENGTH = 10 # maximum supported video length in minutes
 
@@ -61,3 +64,49 @@ def clean_link(link):
     if len(link_split) == 2:
         return link_split[1]
     return link
+
+def convert_mp3_to_wav(path):
+    """Given a relative path to a .mp3 audio file, converts the audio to .wav
+    format. Must export .mp3 to .wav file first, before sampling .wav. Then .wav
+    file can be deleted from directory.
+    Arguments:
+        path: String relative path to .mp3 file
+    Returns: List of sampled audio from .wav file at 16 bits per sample
+    """
+    # Extract the filename of the mp3 from the path
+    filename = path.split("/audio_files/",1)[1]
+    filename = filename.split(".mp3",1)[0]
+    filename_wav = "{}.wav".format(filename)
+    samples = []
+
+    # In some cases the dependency ffmpeg will misread a .mp3 file, and requires a .mp4 container
+    # https://stackoverflow.com/questions/70660431/couldntdecodeerror-decoding-failed-ffmpeg-returned-error-code-69
+    try:
+        sound = AudioSegment.from_file(path, "mp3")
+    except:
+        sound = AudioSegment.from_file(path, format="mp4")
+
+    # Export the sound into a .wav file in the root folder to be read
+    sound.export(filename_wav, format="wav")
+
+    # Use wave to read .wav file and extract samples to array
+    if os.path.exists(filename_wav):
+        # Read all of the frames from the .wav file and place into a bytestring
+        audio_wav = wave.open(filename_wav, 'rb')
+        nframes = audio_wav.getnframes()
+        bytestring = audio_wav.readframes(audio_wav.getnframes())
+
+        # By default, the .wav file is read from at 32 bits at a time, so
+        # unpack into 2 16 bit integers and add to samples both integers
+        for frame in range(nframes):
+            ind = 4*frame
+            samples += list(struct.unpack("<hh", bytestring[ind:ind+4]))
+
+        # Close the connection to the .wav file
+        audio_wav.close()
+
+        # Delete the .wav file as no longer needed
+        os.remove(filename_wav)
+
+    # Return the list of 2-byte integer samples of the audio for future processing/streaming
+    return samples
