a1353a189e85ef4c87e52e263317bdfdbee0ece0
Tristan Culp <tculp@mit.edu>
2022-04-20 23:13:44 -0400

    Add API Routes for queue functionality

--- nicer_rack_web/server/api.py
+++ nicer_rack_web/server/api.py
@@ -5,6 +5,8 @@ from flask import jsonify
 from ...link_handler import extract_link_data
 from ...data_handler import insert_data
 
+queue = []
+
 app = Flask(__name__)
 # NEED THIS FOR WEBSERVER TO FLASK SERVER COMMUNICATION
 CORS(app,resources={r"/*":{"http://localhost:5000/":"*","http://localhost":"*"}})
@@ -23,9 +25,27 @@ def add_link(link=None):
     return {'message': 'Link given, downloaded, and saved in db',
     'title': title, 'duration': duration, 'yt_link': yt_link, 'thumbnail': thumbnail}
 
+@app.route('/all_song_info/')
+def get_song_info():
+    pass
 
 # GET Request
-@app.route('/get_queue')
+@app.route('/get_queue/')
 def get_queue():
-    message = {'greeting': 'Hello from flask'}
-    return message
+    return queue
+
+@app.route('/add_song_queue/<link>')
+@app.route('/add_song_queue/')
+def add_song_queue(link=None):
+    if not link:
+        return {'message': 'No link given'}
+    data = extract_link_data(link)
+    if not data:
+        return {'message': 'Invalid link given'}
+    title, duration, yt_link, filepath, thumbnail = data
+
+    obj = {'title': title, 'thumbnail': thumbnail}
+    queue.append(obj)
+    return obj
+
+
